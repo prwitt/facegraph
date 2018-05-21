fa9873e7881a24f93f12aedbb95d43c23a87348a
Paul Renato <paulrenato@Pauls-MacBook-Pro.local>
2018-05-20 23:33:42 -0700

    Template Update

---
+++ rgcreate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# -e: immediately exit if any command has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+declare deploymentName=""
+declare resourceGroupLocation=""
+
+# Initialize parameters specified from command line
+while getopts ":i:g:n:l:" arg; do
+	case "${arg}" in
+		i)
+			subscriptionId=${OPTARG}
+			;;
+		g)
+			resourceGroupName=${OPTARG}
+			;;
+		n)
+			deploymentName=${OPTARG}
+			;;
+		l)
+			resourceGroupLocation=${OPTARG}
+			;;
+		esac
+done
+
+resourceGroupName="rg-smilemein-automation-qa"
+resourceGroupLocation="westus"
+
+az group create --name $resourceGroupName --location $resourceGroupLocation
+
+#az group deployment create -g $resourceGroupName --template-file stg1.json --parameters stg1-param.json
